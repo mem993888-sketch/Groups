54eacd725cc0791e02752ebfc7bea22e5ddab198
mem993888-sketch <mem993888@gmail.com>
2026-06-06 06:20:28 +0300

    Update MainActivity.kt

--- app/src/main/res/values/app/src/main/java/com/whale/share/MainActivity.kt
+++ app/src/main/res/values/app/src/main/java/com/whale/share/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.whale.share;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.Gravity;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // تعيين ثيم متوافق برمجياً قبل استدعاء السوبر لمنع الانهيار
-        setTheme(androidx.appcompat.R.style.Theme_AppCompat_Light_NoActionBar);
-        
         super.onCreate(savedInstanceState);
         
-        // إنشاء واجهة بسيطة برمجياً للتأكد من عمل التطبيق
+        // إنشاء واجهة بسيطة برمجياً لعرض نص الترحيب
         TextView textView = new TextView(this);
         textView.setText("Whale Share App\nIt Works Perfectly!");
         textView.setTextSize(24);
